a4d088656b8b859b341beb08b7bd8964bc20750f
LinusJacobsson <jacobssonlinus00@gmail.com>
2025-12-09 12:40:38 +0100

    Enhance Value struct with operation tracking and child nodes for arithmetic operations

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,69 @@ use std::ops::Mul;
 use std::ops::MulAssign;
 use std::ops::Div;
 use std::ops::DivAssign;
+use std::collections::HashSet;
 
-#[derive(Debug, Clone, Copy)]
+fn trace<'a>(root: &'a Value) -> (Vec<&'a Value>, Vec<(&'a Value, &'a Value)>) {
+    fn build<'a>(
+        v: &'a Value,
+        nodes: &mut Vec<&'a Value>,
+        edges: &mut Vec<(&'a Value, &'a Value)>,
+        seen: &mut HashSet<*const Value>,
+    ) {
+        let ptr = v as *const Value;
+        if !seen.insert(ptr) {
+            return;
+        }
+        nodes.push(v);
+        for child in &v._children {
+            edges.push((child, v));
+            build(child, nodes, edges, seen);
+        }
+    }
+
+    let mut nodes = Vec::new();
+    let mut edges = Vec::new();
+    let mut seen = HashSet::new();
+    build(root, &mut nodes, &mut edges, &mut seen);
+    (nodes, edges)
+}
+fn to_dot(root: &Value, rankdir: &str) -> String {
+    assert!(matches!(rankdir, "LR" | "TB"));
+
+    let (nodes, edges) = trace(root);
+    let mut s = String::new();
+    s.push_str("digraph G {\n");
+    s.push_str(&format!("  rankdir={};\n", rankdir));
+    s.push_str("  node [shape=record];\n");
+
+    for n in &nodes {
+        let id = format!("{:p}", *n as *const Value);
+        s.push_str(&format!("  \"{}\" [label=\"{{ data {} }}\"];\n", id, n.data));
+
+        if let Some(op) = &n._operation {
+            let op_id = format!("{}{}", id, op);
+            s.push_str(&format!("  \"{}\" [label=\"{}\"];\n", op_id, op));
+            s.push_str(&format!("  \"{}\" -> \"{}\";\n", op_id, id));
+        }
+    }
+
+    for (n1, n2) in &edges {
+        if let Some(op) = &n2._operation {
+            let from = format!("{:p}", *n1 as *const Value);
+            let to = format!("{}{}", format!("{:p}", *n2 as *const Value), op);
+            s.push_str(&format!("  \"{}\" -> \"{}\";\n", from, to));
+        }
+    }
+
+    s.push_str("}\n");
+    s
+}
+
+#[derive(Debug)]
 struct Value {
     data: i32,
+    _children: Vec<Value>,
+    _operation: Option<String>,
 }
 
 impl Add for Value {
@@ -18,6 +77,8 @@ impl Add for Value {
     fn add(self, other: Self) -> Self {
         Value {
             data: self.data + other.data,
+            _children: vec![self, other],
+            _operation: Some(String::from("+")),
         }
     }
 }
@@ -25,6 +86,7 @@ impl Add for Value {
 impl AddAssign for Value {
     fn add_assign(&mut self, other: Self) {
         self.data += other.data;
+
     }
 
 }
@@ -35,6 +97,8 @@ impl Sub for Value {
     fn sub(self, other: Self) -> Self {
         Value {
             data: self.data - other.data,
+            _children: vec![self, other],
+            _operation: Some(String::from("-")),
         }
     }
 }
@@ -51,6 +115,8 @@ impl Mul for Value {
     fn mul(self, other: Self) -> Self {
         Value {
             data: self.data * other.data,
+            _children: vec![self, other],
+            _operation: Some(String::from("*")),
         }
     }
 }
@@ -67,6 +133,8 @@ impl Div for Value {
     fn div(self, other: Self) -> Self {
         Value {
             data: self.data / other.data,
+            _children: vec![self, other],
+            _operation: Some(String::from("/")),
         }
     }
 }
@@ -79,12 +147,13 @@ impl DivAssign for Value {
 
 fn main() {
     println!("Hello, world!");
-    let value_1 = Value { data: 10 };
-    let value_2 = Value { data: 5 };
+    let value_1 = Value { data: 10, _children: vec![], _operation: None };
+    let value_2 = Value { data: 5, _children: vec![], _operation: None };
     let sum = value_1 + value_2;
-    let difference = value_1 - value_2;
     println!("Sum: {:?}", sum);
-    println!("Difference: {:?}", difference);
+    let dot = to_dot(&sum, "LR");
+    std::fs::write("graph.dot", dot).unwrap();
+    println!("Wrote compute graph to graph.dot");
 }
 
 
